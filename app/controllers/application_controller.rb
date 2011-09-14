7bedb569f01119635e268ea71eb53bb9194d32d0
Oge Nnadi <oge@nnadi.org>
2011-09-14 13:10:50 +0100

    fleshed out the search page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_variables
+  
+  def set_variables
+    @igbo_count     = IgboEntry.count
+    @english_count  = EnglishEntry.count
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   def dictionary
     @q        = params[:q]
     @dict     = params[:dict]
-    klass     = params[:dict] == 'ig-en' ? IgboEntry : EnglishEntry
-    @results  = klass.where('term LIKE ?', "#{@q}%").paginate(:page => params[:page])
+    
+    if @q
+      klass     = params[:dict] == 'ig-en' ? IgboEntry : EnglishEntry
+      @results  = klass.where('term LIKE ?', "#{@q}%").paginate(:page => params[:page])
+    end
   end
 end
