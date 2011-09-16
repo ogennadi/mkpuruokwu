683320aaf4861f8f11af15de64cc63b53ede70c0
Oge Nnadi <oge@nnadi.org>
2011-09-16 22:36:59 +0100

    added english text2csv parser

--- lib/cleaner.rb
+++ lib/cleaner.rb
@@ -1,13 +1,81 @@
 ﻿require 'csv'
 
 class Cleaner
-  LANGUAGE, TERM, TERM_CLASS, TONES, DEFINITION  = 0, 1, 2, 3, 4
+  LANGUAGE, TERM, TERM_CLASS, TONES, DEFINITION, SOURCE  = 0, 1, 2, 3, 4, 5
   
   def self.csv2db(path)     
     CSV.foreach(path) do |row|
-      IgboEntry.create(:term => row[TERM], :term_class => row[TERM_CLASS], :tones => row[TONES], :definition => row[DEFINITION])
-      puts "created #{row[TERM]}"
+      if row[LANGUAGE] == 'ig'
+        IgboEntry.create(:term => row[TERM], :term_class => row[TERM_CLASS], :tones => row[TONES], :definition => row[DEFINITION])
+       else
+         EnglishEntry.create(:term => row[TERM], :term_class => row[TERM_CLASS], :definition => row[DEFINITION])
+       end
+       
+       puts "#{row[LANGUAGE]} #{row[TERM]}"
     end
+  end
+  
+  def self.en2csv(path)
+    file    = File.new(path, 'r')
+    content = file.read
+    counter = 0
+    regex   = /[^\.]+\./
+    
+    CSV.open('public/dict2.csv', 'w') do |csv|
+      while match = regex.match(content)
+        entry = match[0]
+        
+        buffer = nil
+        entry.lines.each do |line|
+          if buffer.nil?
+            buffer = line
+          elsif /[^\[\]\s]+\[.+\]\s*/.match line
+            csv << write_en(buffer)
+            counter = counter + 1
+            buffer = line
+          else
+            buffer = buffer + line
+          end
+        end
+        
+        unless buffer.nil?
+          content = content.sub(entry, '')
+          csv << write_en(buffer)
+          counter = counter + 1
+        end
+      end
+    end
+    
+    puts counter
+    file.close
+  end
+  
+  def self.write_en(buffer)
+    splitted = if /\s+n\s+/.match buffer
+          term_class = 'n'
+      buffer.split(/\s+n\s+/)
+
+    elsif /\s+v\s+/.match buffer
+                term_class = 'v'
+      buffer.split(/\s+v\s+/)
+
+    elsif /\s+adj\s+/.match buffer
+                term_class = 'adj'
+      buffer.split(/\s+adj\s+/)
+
+    elsif /\s+adv\s+/.match buffer
+      term_class = 'adv'
+      buffer.split(/\s+adv\s+/)
+
+    else
+      [buffer]
+    end  
+    
+    term = splitted.first.sub(/^\s+/, '')
+    definition =  splitted[1..-1].join
+    puts '------------------------------------------'
+    puts term
+    return ['en', term, term_class, nil, definition, 'echeruo']
   end
   
   def self.ig2csv(path)
@@ -17,40 +85,40 @@ class Cleaner
     regex   = /(\A[^\[\n]+)\[([^\]]+)\](.*\z)/m
     
     CSV.open('public/dict.csv', 'w') do |csv|
-    while (line = file.gets)
-      if buffer.nil?
-        buffer = line
-      elsif line.first.blank? || !regex.match(line)
-        buffer = buffer + line
-      else
-        matches     = regex.match(buffer)
-        
-        if matches
-          pretones        = matches[1]
-          tones           = matches[2]
-          definition      = matches[3]
-          split_pretones  = pretones.split
-          
-          if split_pretones.length > 1
-            term_class = split_pretones.last
-            term = split_pretones[0..-2].join(" ")
-          end
-          
-          puts term
-          puts term_class
-          puts tones
-          puts definition
-          csv << ["ig", term, term_class, tones, definition]
+      while (line = file.gets)
+        if buffer.nil?
+          buffer = line
+        elsif line.first.blank? || !regex.match(line)
+          buffer = buffer + line
         else
-        
-          puts buffer
-          csv << ["ig", buffer]
+          matches     = regex.match(buffer)
+          
+          if matches
+            pretones        = matches[1]
+            tones           = matches[2]
+            definition      = matches[3]
+            split_pretones  = pretones.split
+            
+            if split_pretones.length > 1
+              term_class = split_pretones.last
+              term = split_pretones[0..-2].join(" ")
+            end
+            
+            puts term
+            puts term_class
+            puts tones
+            puts definition
+            csv << ["ig", term, term_class, tones, definition, "echeruo"]
+          else
+          
+            puts buffer
+            csv << ["ig", buffer, "echeruo"]
+          end
+          puts "---------------------------------------------------------"
+          counter = counter + 1
+          buffer = line
         end
-        puts "---------------------------------------------------------"
-        counter = counter + 1
-        buffer = line
       end
-    end
     end
     
     puts counter
