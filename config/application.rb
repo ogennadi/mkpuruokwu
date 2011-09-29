c31851fe5755f1ebb719a606b9e6df7bbb70325d
Oge Nnadi <oge@nnadi.org>
2011-09-29 08:29:44 +0100

    made www redirect to non-www URL

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Igbo
   class Application < Rails::Application
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
-
+    config.middleware.use "WwwDitcher"
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
